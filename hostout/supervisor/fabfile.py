dfa99aa2a14749fc6323dcc865c5fc0fa4076700
Dylan Jay <software@pretaweb.com>
2010-12-21 01:02:57 +1100

    reload rather than shutdown

--- hostout/supervisor/fabfile.py
+++ hostout/supervisor/fabfile.py
@@ -1,6 +1,8 @@
 import os
 import os.path  #import os.path.join, os.path.basename, os.path.dirname
 from fabric import api, contrib
+from collective.hostout.hostout import buildoutuser
+
 
 initd = """
 #!/bin/sh
@@ -189,24 +191,29 @@ def predeploy():
 
 def postdeploy():
     supervisorstartup()
-    
+        
 def supervisorstartup():
     """Start the supervisor daemon"""
     hostout = api.env.hostout
     path = hostout.getRemoteBuildoutPath()
     bin = "%(path)s/bin" % locals()
     supervisor = hostout.options['supervisor']
-    api.sudo("%(bin)s/%(supervisor)sd"% locals())
-    api.sudo("%(bin)s/%(supervisor)sctl status"% locals())
+    try:
+        api.run("%(bin)s/%(s)sctl reread; %(bin)s/%(s)sctl start all" %dict(bin=bin,s=supervisor))
+    except:
+        api.sudo("%(bin)s/%(supervisor)sd"% locals())
+    api.run("%(bin)s/%(supervisor)sctl status"% locals())
 
+@buildoutuser
 def supervisorshutdown():
     """Shutdown the supervisor daemon"""
     hostout = api.env.hostout
     path = hostout.getRemoteBuildoutPath()
     bin = "%(path)s/bin" % locals()
     supervisor = hostout.options['supervisor']
-    api.sudo("%(bin)s/%(supervisor)sctl shutdown || echo 'Failed to shutdown'"% locals() )
+    api.run("%(bin)s/%(supervisor)sctl stop all || echo 'Failed to shutdown'"% locals() )
 
+@buildoutuser
 def supervisorctl(*args):
     """Runs remote supervisorlctl with given args"""
     hostout = api.env.hostout
@@ -219,19 +226,24 @@ def supervisorctl(*args):
     api.run("%(bin)s/%(supervisor)sctl %(args)s" % locals())
 
 
+@buildoutuser
 def restart(*args):
     """ supervisorctl restart command """
     api.env.hostout.supervisorctl('restart', *args)
 
+@buildoutuser
 def start(*args):
     """ supervisorctl start command """
     api.env.hostout.supervisorctl('start', *args)
+@buildoutuser
 def stop(*args):
     """ supervisorctl stop command """
     api.env.hostout.supervisorctl('stop', *args)
+@buildoutuser
 def status(*args):
     """ supervisorctl status command """
     api.env.hostout.supervisorctl('status', *args)
+@buildoutuser
 def tail(*args):
     """ supervisorctl tail command """
     api.env.hostout.supervisorctl('tail', *args)
